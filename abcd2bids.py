af0e113375a40b30dc08efd1a363e55d976d23b5
Mia Zwally <132242817+MIZwally@users.noreply.github.com>
2023-09-22 15:30:23 -0400

    Updated `SESSIONS`
    Added years 4 and 6 as timepoints in the list of sessions available for download to reflect current state of dataset.

--- abcd2bids.py
+++ abcd2bids.py
@@ -58,7 +58,7 @@ TEMP_FILES_DIR = os.path.join(PWD, "temp")
 UNPACK_AND_SETUP = os.path.join(PWD, "src", "unpack_and_setup.sh")
 UNPACKED_FOLDER = os.path.join(PWD, "data")
 MODALITIES = ['anat', 'func', 'dwi']
-SESSIONS = ['baseline_year_1_arm_1', '2_year_follow_up_y_arm_1']
+SESSIONS = ['baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_arm_1', '6_year_follow_up_arm_1']
 
 
 def main():
